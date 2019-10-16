e8539aea605207278c6eba46ef1c010237706b73
Nikhilesh Chaudhary <nikhileshchaudhary@Nikhileshs-MacBook-Pro.local>
2019-10-15 17:50:47 -0700

    first commit

---
+++ simple_cms/app/controllers/demo_controller.rb
@@ -0,0 +1,18 @@
+class DemoController < ApplicationController
+  layout false
+  def index
+    render('index')
+  end
+
+  def hello
+    render('hello')
+  end
+
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def lynda
+    redirect_to('https://www.lynda.com')
+  end
+end
